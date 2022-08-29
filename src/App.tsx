7c27c27c561810c1b8ddda1c674c6724ab115056
nikmace <nikmacentosh@gmail.com>
2022-08-29 18:36:24 +0300

    Added ability to use sass and tailwind together

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const App: React.FC = () => {
-  return <div>Hello from React!</div>;
+  return (
+    <div>
+      <h1 className="text-red-300 text-xl">Hello from React!</h1>
+    </div>
+  );
 };
 
 export default App;
